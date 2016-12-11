2e4240038b21cf256c1f9c4d765b7ecadb2ad71f
roslanelyashiv <roslane@shellanoo.com>
2016-12-11 23:30:24 +0200

    # infinite scroll start

--- src/pages/Vuejs.vue
+++ src/pages/Vuejs.vue
@@ -30,6 +30,8 @@
 
         </mu-card>
       </mu-col>
+      <br>
+
     </mu-row>
 
 
@@ -48,6 +50,8 @@
 
       return {
         posts: [],
+
+        busy: false,
         object: {
           FirstName: 'John',
           LastName: 'Doe',
@@ -60,24 +64,16 @@
       this.fetchPosts();
     },
 
-    filters: {
-      limit: function(arr, limit) {
-        return arr.slice(0, Number(limit))
-      }
-    },
-
-
 
     methods: {
       fetchPosts: function(){
 
-        this.loading = true;
+        //this.loading = true;
 
         this.$http.get('../src/api/api.json').then((response) => {
 
           this.loading = false;
           this.posts = response.data;
-
         }, (errorResponse) => {
 
           // Handle error...
@@ -89,6 +85,7 @@
       limit: function(posts,number) {
         return this.posts.slice(0, number)
       }
+
     }
 
 
